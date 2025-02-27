56b0ab21673248a9ac13ddb48df2b26d0703ffc0
egan-ombaka <eganombaka97@gmail.com>
2025-02-27 11:36:30 +0300

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Github from "./components/Github"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Work from "./components/Work"
 
 function App() {
   return <div>
     <Header />
     <Hero />
     <Github />
+    <Work />
   </div>
 }
 export default App
---
+++ src/components/Work.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../css/Work.css'
+
+const Work = () => {
+  return (
+    <div className='work-experience'>
+      <div className='work-container'>
+        <h4 className='work'>Work Exprience</h4>
+        <div className='mac'>
+          <div>eg</div>
+          <div>supra</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Work
