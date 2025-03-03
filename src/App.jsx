9bd7de82b3ce2b538bec67dcc1f6d1211be455b3
egan-ombaka <eganombaka97@gmail.com>
2025-03-03 10:00:06 +0300

    Techstack section

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,26 @@ import Hero from "./components/Hero"
 import MovingIcons from "./components/MovingIcons"
 import Work from "./components/Work"
 
+const icons = [
+  {src: 'icons/react.svg', alt: 'React.JS'},
+  {src: 'icons/icon2.svg', alt: 'Javascript'},
+  {src: 'icons/icon3.svg', alt: 'Python'},
+  {src: 'icons/icon4.svg', alt: 'React.JS'},
+  {src: 'icons/icon5.svg', alt: 'React.JS'},
+  {src: 'icons/icon6.svg', alt: 'React.JS'},
+  {src: 'icons/icon7.svg', alt: 'React.JS'},
+  {src: 'icons/linux.svg', alt: 'React.JS'},
+  {src: 'icons/figma.svg', alt: 'React.JS'},
+  {src: 'icons/docker.svg', alt: 'React.JS'},
+]
+
 function App() {
   return <div>
-    <MovingIcons />
     <Header />
     <Hero />
     <Github />
     <Work />
+    <MovingIcons icons={icons}/>
   </div>
 }
 export default App
--- src/components/MovingIcons.jsx
+++ src/components/MovingIcons.jsx
@@ -1,39 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "../css/MovingIcons.css";
-import Icon1 from "../assets/icon1.svg";
-import Icon2 from "../assets/icon2.svg";
-import Icon3 from "../assets/icon3.svg";
-import Icon4 from "../assets/icon4.svg";
-import Icon5 from "../assets/icon5.svg";
-import Icon6 from "../assets/icon6.svg";
-import Icon7 from "../assets/icon7.svg";
 
-const MovingIcons = () => {
-  useEffect(() => {
-    const iconsContainer = document.querySelector(".moving-icons");
-    const icons = [Icon1, Icon2, Icon3, Icon4, Icon5, Icon6, Icon7];
-    const iconsCount = 12; // Number of icons to generate
-
-    for (let i = 0; i < iconsCount; i++) {
-      const icon = document.createElement("img");
-      icon.setAttribute("class", "icon");
-      icon.setAttribute("src", icons[Math.floor(Math.random() * icons.length)]);
-
-      iconsContainer.appendChild(icon);
-
-      const duration = Math.random() * 20 + 10;
-      const delay = Math.random() * 10;
-      const top = Math.random() * 100;
-      const left = Math.random() * 100;
-
-      icon.style.animationDuration = `${duration}s`;
-      icon.style.animationDelay = `${delay}s`;
-      icon.style.top = `${top}%`;
-      icon.style.left = `${left}%`;
-    }
-  }, []);
-
-  return <div className="moving-icons"></div>;
+const MovingIcons = ({ icons }) => {
+  return (
+    <div className="techstack-section">
+      <div className="techstack-container">
+        {icons.map((icon, index) => (
+          <img
+            key={index}
+            className="tech-logo"
+            src={icon.src}
+            alt={icon.alt}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MovingIcons;
