fc9d08675b34f03b35320fb90f10af1b889de1dd
egan-ombaka <eganombaka97@gmail.com>
2025-03-15 10:19:04 +0300

    blog section update

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import '../css/Blog.css'
 
 const Blog = () => {
   return (
-    <div>
-      
+    <div className='blogsection'>
+      <img className='warning' src="images/warning.svg" alt="under...." />
+      <h6 className='warning-text'>Sorry!! This page is under construction</h6>
     </div>
   )
 }
