c2bccb6e9ce3a248340d0eede9b8931cd111173d
egan-ombaka <eganombaka97@gmail.com>
2025-03-15 10:36:55 +0300

    dropdown toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Header.css";
 
 const Header = ({ setCurrentPage }) => {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+
   return (
     <div className="header-section">
       <div className="header">
@@ -21,8 +27,15 @@ const Header = ({ setCurrentPage }) => {
           </li>
         </ul>
 
-        <div className="contact-icon">
-          <img className="contact-svg" src="icons/contact.svg" alt="Contact" />
+        <div className="contact-icon" onClick={toggleDropdown}>
+          <img className="contact-svg" src="icons/contact.svg" alt="Connect" />
+          {isDropdownVisible && (
+            <div className="dropdown-menu">
+              <a href="#contact1">Contact 1</a>
+              <a href="#contact2">Contact 2</a>
+              <a href="#contact3">Contact 3</a>
+            </div>
+          )}
         </div>
       </div>
     </div>
